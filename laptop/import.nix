5e3e080f42f3f5602f5201047d54eceda3ea853b
Leon Hubrich <mail@leon-hubrich.de>
2024-08-10 04:43:30 +0200

    add hardware support for my laptop

--- laptop/import.nix
+++ laptop/import.nix
@@ -1,6 +1,6 @@
 {
   imports =
     [
-
+      "${builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }}/lenovo/legion/15arh05h" # Compatible with Lenovo Ideapad Gaming 3, 16ARH07
     ];
 }
