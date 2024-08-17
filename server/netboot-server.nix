a7aea1f938d242f96980534e6503d7cb25742264
Leon Hubrich <mail@leon-hubrich.de>
2024-08-17 18:04:36 +0200

    Netboot

---
+++ server/netboot-server.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+{
+  services.tftpd.enable = true;
+  services.atftpd.root = "/srv/netboot"
+
+    services.nginx.enable = true;
+
+  services.nginx.virtualHosts."netboot.int.onlh.de" = {
+    root = "/srv/netboot";
+  };
+
+
+  # Dependencys of IPXE
+  environment.systemPackages = with pkgs; [
+    gnu-efi
+    mtools
+    openssl
+    perl
+    xorriso
+    xz
+  ];
+}
