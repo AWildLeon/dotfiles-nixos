a1a9fcb16237e67cec19c0a6096d08a6a0f5b709
Leon Hubrich <46073213+Games-Crack@users.noreply.github.com>
2024-09-22 01:42:13 +0200

    enable ipv6 support

--- server/docker.nix
+++ server/docker.nix
@@ -5,6 +5,12 @@
     liveRestore = false;
     autoPrune.enable = true;
     package = pkgs.docker_27;
+    daemon.settings = {
+      ipv6 = true;
+      experimental = true;
+      fixed-cidr-v6 = "fd00::/80";
+      ip6tables = true;
+    };
   };
 
   boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 0;
